25ec00b0d2f309ecc8d302b7d63e26fd5a872184
Ersin <112959117+ersin98@users.noreply.github.com>
2023-04-15 00:29:37 +0300

    readable

--- globalConstants.py
+++ globalConstants.py
@@ -1,33 +1,25 @@
-class GlobalConstants():
-    mainURL="http://localhost:8080/swagger-ui/index.html#/"
+class Result:
     ok="200"
     badRequest="400"
     created="201"
     undocumented="500"
-
-    #writingArea
+class Category:
+    add="#operations-categories-controller-addCategory .opblock-summary-control"
+    get="#operations-categories-controller-getAll_1 .opblock-summary-control"
+    deleteAll="#operations-categories-controller-deleteAll_1 .opblock-summary-control"
+    update="#operations-categories-controller-updateCategory .opblock-summary-control"
+    delete="#operations-categories-controller-deleteCategory .opblock-summary-path span"
+class Product:
+    getAll = "#operations-products-controller-getAll .opblock-summary-control"
+    update="#operations-products-controller-updateProduct .opblock-summary-control"
+    add="#operations-products-controller-addProduct .opblock-summary-control"
+    deleteAll="#operations-products-controller-deleteAll .opblock-summary-control"
+    getByQuery="#operations-products-controller-getByQueryProductResponse .opblock-summary-control"
+    getByCategory="#operations-products-controller-getByCategoryProductResponse .opblock-summary-path span"
+    delete="#operations-products-controller-deleteProduct .opblock-summary-path span"
+class Write:
+    addCategoryExample="{\"name\":\"example\"}"
+    #addProductExaple="{\"description\":\"example\",\"image\":\"example\",\"price\":9.99,\"title\":\"example\",\"categoryId\":5}"
     body=".body-param__text"
     parameters=".parameters-col_description > input"
-    
-
-    #category
-    addCategory="#operations-categories-controller-addCategory .opblock-summary-control"
-    getCategories="#operations-categories-controller-getAll_1 .opblock-summary-control"
-    deleteCategories="#operations-categories-controller-deleteAll_1 .opblock-summary-control"
-    updateCategory="#operations-categories-controller-updateCategory .opblock-summary-control"
-    deleteCategory="#operations-categories-controller-deleteCategory .opblock-summary-path span"
-    #product
-    getAll = "#operations-products-controller-getAll .opblock-summary-control"
-    updateProduct="#operations-products-controller-updateProduct .opblock-summary-control"
-    addProduct="#operations-products-controller-addProduct .opblock-summary-control"
-    deleteAll="#operations-products-controller-deleteAll .opblock-summary-control"
-    getByQueryProductResponse="#operations-products-controller-getByQueryProductResponse .opblock-summary-control"
-    getByCategoryProductResponse="#operations-products-controller-getByCategoryProductResponse .opblock-summary-path span"
-    deleteProduct="#operations-products-controller-deleteProduct .opblock-summary-path span"
-    
-
-
-    addCategoryExample="{\"name\":\"example\"}"
-    addProductExaple="{\"description\":\"example\",\"image\":\"example\",\"price\":9.99,\"title\":\"example\",\"categoryId\":5}"
-
-
+    queryExample="exaple"
--- test_swagger.py
+++ test_swagger.py
@@ -1,42 +1,44 @@
-from msilib.schema import ServiceControl
-from selenium import webdriver
+from selenium.webdriver.support.expected_conditions import staleness_of
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+import pytest
+from datetime import date
 from time import sleep
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-from selenium.webdriver.common.action_chains import ActionChains
-import pytest
 from pathlib import Path
 from datetime import datetime
-from datetime import date
 import openpyxl 
-from selenium.webdriver.support.ui import WebDriverWait as pagewait
-from selenium.webdriver.support.expected_conditions import staleness_of
-from globalConstants import GlobalConstants as GC
-
+from msilib.schema import ServiceControl
+from globalConstants import Category
+from globalConstants import Product
+from globalConstants import Result
+from globalConstants import Write
 class Test_Localhost:
     vars = {}
     idNumber="0"
     def setup_method(self):
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome() # webdriver.Chrome(ChromeDriverManager().install())
         self.driver.maximize_window()
         now = datetime.now()
         self.folderPath = str(now.strftime("%d-%b-%Y")) #str(date.today())
         self.testTime=str(now.strftime("%H.%M")) #.%S
         Path(self.folderPath).mkdir(exist_ok=True)
         self.driver.get("http://localhost:8080/swagger-ui/index.html#/")
+        self.runController(Product.deleteAll,())
+        self.runController(Category.deleteAll,())
+
+        
     def teardown_method(self):
         self.vars = {}#sadece test bitiminde sıfırlanır metot bitiminde değil
+        self.runController(Product.deleteAll,())
+        self.runController(Category.deleteAll,())
         self.driver.quit()
 
-    def errormessage(self,text,position):
-        self.waitForElementVisible((By.CSS_SELECTOR, position))
-        errormessage = self.driver.find_element(By.CSS_SELECTOR, position)
-        assert errormessage.text == f"\"{text}\"" 
+
 
     def getData(excelFileName,sheetName):
         excelFile = openpyxl.load_workbook("data/"+excelFileName)
@@ -55,31 +57,56 @@ class Test_Localhost:
             else:
                 data.append(cell_value)
         return data
-    
-    def result(self,resultNumer):
-        self.waitForElementVisible((By.CSS_SELECTOR,".live-responses-table .response > .response-col_status"))
-        assert self.driver.find_element(By.CSS_SELECTOR, ".live-responses-table .response > .response-col_status").text == resultNumer
+
 
     def waitForElementVisible(self,locator,timeout=10):
         WebDriverWait(self.driver,timeout).until(ec.visibility_of_element_located(locator))
 
-    def getCategoryId(self):#self.vars["id"]
+    ######################################################################Make ID##########################################
+    def getCategoryId(self):#only for makeCategoryIdNumber
         self.vars["categoryId"]= self.driver.find_element(By.CSS_SELECTOR, ".microlight:nth-child(3) span:nth-child(5)").text
         self.categoryIdNumber= self.vars["categoryId"]
-        
-    def getProductId(self):
+    def getProductId(self):#only for makeProductIdNumber
         self.vars["ProductId"]= self.driver.find_element(By.CSS_SELECTOR, ".microlight:nth-child(3) span:nth-child(10)").text
-        self.productIdNumber= self.vars["ProductId"]    
-        
+        self.productIdNumber= self.vars["ProductId"]  
 
-#Controller
-    def startController(self,opblockSummaryControl):
+    def makeCategoryIdNumber(self):
+        self.runController(Category.deleteAll,())
+        self.runController(Category.add,(),Write.body,Write.addCategoryExample)
+        self.runController(Category.get,(
+            lambda: self.getCategoryId()
+        ))
+    def makeProductIdNumber(self):
+        self.runController(Product.deleteAll,())
+        self.makeCategoryIdNumber()
+        addProductExaple="{\"description\":\"example\",\"image\":\"example\",\"price\":9.99,\"title\":\"example\","+f"\"categoryId\":{self.categoryIdNumber}"+"}"
+        self.runController(Product.add,(),Write.body, addProductExaple)
+        self.runController(Product.getAll,(
+            lambda: self.getProductId()
+        ))
+
+########function list#########
+    def result(self,resultNumer):
+        self.waitForElementVisible((By.CSS_SELECTOR,".live-responses-table .response > .response-col_status"))
+        assert self.driver.find_element(By.CSS_SELECTOR, ".live-responses-table .response > .response-col_status").text == resultNumer
+    def errormessage(self,text,position):
+        self.waitForElementVisible((By.CSS_SELECTOR, position))
+        errormessage = self.driver.find_element(By.CSS_SELECTOR, position)
+        assert errormessage.text == f"\"{text}\"" 
+#+ save_screenshot
+
+
+
+
+
+#############################################################runController###########################
+    def startController(self,opblockSummaryControl):#only for runController
         self.waitForElementVisible((By.CSS_SELECTOR, opblockSummaryControl))
         self.driver.find_element(By.CSS_SELECTOR, opblockSummaryControl).click()
         self.waitForElementVisible((By.CSS_SELECTOR, ".try-out__btn"))
         self.driver.find_element(By.CSS_SELECTOR, ".try-out__btn").click()
 
-    def stopController(self,opblockSummaryControl):
+    def stopController(self,opblockSummaryControl):#only for runController
         self.waitForElementVisible((By.CSS_SELECTOR, opblockSummaryControl))
         self.waitForElementVisible((By.CSS_SELECTOR, ".btn-clear"))
         self.waitForElementVisible((By.CSS_SELECTOR, ".btn"))
@@ -92,22 +119,7 @@ class Test_Localhost:
             functions = (functions,)
             return functions
         return functions
-
-    def makeCategoryIdNumber(self):
-        self.runController(GC.deleteCategories,())
-        self.runControllerWithInput(GC.addCategory,GC.body,GC.addCategoryExample,())
-        self.runController(GC.getCategories,(
-            lambda: self.getCategoryId()
-        ))
-
-    def makeProductIdNumber(self):
-        self.runController(GC.deleteAll,())
-        self.makeCategoryIdNumber()
-        addProductExaple="{\"description\":\"example\",\"image\":\"example\",\"price\":9.99,\"title\":\"example\","+f"\"categoryId\":{self.categoryIdNumber}"+"}"
-        self.runControllerWithInput(GC.addProduct,GC.body,addProductExaple,())
-        self.runController(GC.getAll,(
-            lambda: self.getProductId()
-        ))
+    
     def write(self,writingArea,input):
         self.waitForElementVisible((By.CSS_SELECTOR, writingArea))
         bodyParam = self.driver.find_element(By.CSS_SELECTOR, writingArea)
@@ -115,77 +127,61 @@ class Test_Localhost:
         bodyParam.clear()
         bodyParam.send_keys(input)
 
-#ekle getir gibi fonksiyonlarının yerine body kullanacak ya da kullanmayacak controller işlemini çağırıyoruz.
-    def runController(self,controller,functions):#hiç parametre almıyor
+    def runController(self,controller,functions,writingArea=None,input=None):#hiç parametre almıyor
         functions = self.callable(functions)
         self.startController(controller)
-
-        self.waitForElementVisible((By.CSS_SELECTOR, ".execute"))
-        self.driver.find_element(By.CSS_SELECTOR, ".execute").click()
-        self.waitForElementVisible((By.CSS_SELECTOR, ".btn-clear"))
-        for function in functions:
-            function()
-
-        self.stopController(controller)
-    
-    def runControllerWithInput(self,controller,writingArea,input,functions):#body ile parametre veriyor
-        functions = self.callable(functions)
-        self.startController(controller)#GC.controllerAddCategory
-
-        self.write(writingArea,input)
-
+        if writingArea and input:#write
+            self.write(writingArea,input)
         self.waitForElementVisible((By.CSS_SELECTOR, ".execute"))
         self.driver.find_element(By.CSS_SELECTOR, ".execute").click()
         self.waitForElementVisible((By.CSS_SELECTOR, ".btn-clear"))
         for function in functions:#ekran görüntüsü alma ve test başarısı işlemleri gibi fonksiyonlar
             function()
-        self.stopController(controller)
+        self.stopController(controller)  
 
-#tests Categories
+
+
+
+
+#############################################################Tests Categories##############################################
     def test_deleteCategories(self):
-        self.runController(GC.deleteCategories,
+        self.runController(Category.deleteAll,
             (
-            lambda: self.result(GC.ok),
+            lambda: self.result(Result.ok),
             lambda: self.driver.save_screenshot(f"{self.folderPath}/ {self.testTime}-test-retrofitSelenium-deleteCategories.png")
             )
         )
 
     def test_addCategory(self):
-        self.runController(GC.deleteCategories,())
-        self.runControllerWithInput(GC.addCategory,GC.body,GC.addCategoryExample,
+        self.runController(Category.add,
             (
-            lambda:self.result(GC.created),
+            lambda:self.result(Result.created),
             lambda:self.driver.save_screenshot(f"{self.folderPath}/ {self.testTime}-test-retrofitSelenium-addCategory.png")
-            )    
+            ),Write.body,Write.addCategoryExample
         )
         
     @pytest.mark.parametrize("name", getData("category_send_keys.xlsx","CreateCategoryRequest"))
     def test_addCategory_invalid(self,name):
-        self.runController(GC.deleteCategories,())
-
         invalidInput="{"+ f"\"name\":\"{name}\""+"}"
-
-        self.runControllerWithInput(GC.addCategory,GC.body,invalidInput,
+        self.runController(Category.add,
             (
             lambda:self.driver.save_screenshot(f"{self.folderPath}/ {self.testTime}-test-retrofitSelenium-addCategory-invalid.png"),
-            lambda:self.result(GC.badRequest),
+            lambda:self.result(Result.badRequest),
             lambda:self.errormessage("boyut '3' ile '20' arasında olmalı",".language-json > span:nth-child(13)")#hata mesajını excel tablosundan çekmeyi unutma
-            )
+            ),Write.body,invalidInput
         )
 
     def test_addCategory_repeat(self):
-        self.runController(GC.deleteCategories,())
-        self.runControllerWithInput(GC.addCategory,GC.body,GC.addCategoryExample,())
-        self.runControllerWithInput(GC.addCategory,GC.body,GC.addCategoryExample,(
-            lambda:self.result(GC.badRequest),
+        self.runController(Category.add,(),Write.body,Write.addCategoryExample)
+        self.runController(Category.add,(
+            lambda:self.result(Result.badRequest),
             lambda:self.errormessage("Category name already exists",".microlight:nth-child(3) span:nth-child(5)"),
             lambda:self.driver.save_screenshot(f"{self.folderPath}/ {self.testTime}-test-retrofitSelenium-addCategory-Repeat.png")
-        ))
-        self.runController(GC.deleteCategories,())
+        ),Write.body,Write.addCategoryExample)
 
     def test_getCategories(self):
-        self.runController(GC.getCategories,(
-            lambda:self.result(GC.ok),
+        self.runController(Category.get,(
+            lambda:self.result(Result.ok),
             lambda:self.driver.save_screenshot(f"{self.folderPath}/ {self.testTime}-test-retrofitSelenium-getCategories.png")
         ))
 
@@ -193,69 +189,64 @@ class Test_Localhost:
     def test_updateCategory(self):
         self.makeCategoryIdNumber()
         updateInput="{"+f"\"id\": {self.categoryIdNumber},\"name\": \"example\""+"}"
-        self.runControllerWithInput(GC.updateCategory,GC.body,updateInput,(
-            lambda:self.result(GC.ok),
+        self.runController( Category.update,(
+            lambda:self.result(Result.ok),
             lambda:self.driver.save_screenshot(f"{self.folderPath}/ {self.testTime}-test-retrofitSelenium-updateCategories.png")
-        ))
-        self.runController(GC.deleteCategories,())
-    
+        ),Write.body,updateInput)
+
     def test_deleteCategory(self):
         self.makeCategoryIdNumber()
-
-        self.runControllerWithInput(GC.deleteCategory,GC.parameters,self.categoryIdNumber,(
+        self.runController(Category.delete,(
             lambda:self.driver.save_screenshot(f"{self.folderPath}/ {self.testTime}-test-retrofitSelenium-deleteCategory.png"),
-            lambda:self.result(GC.ok)
-        ))
-        self.runController(GC.deleteCategories,())
+            lambda:self.result(Result.ok)
+        ),Write.parameters, self.categoryIdNumber)
 
-#tests Products
+
+
+#################################################################Tests Products########################################
     def test_getAll(self):
-        self.runController(GC.getAll,(
+        self.runController(Product.getAll,(
             lambda:self.driver.save_screenshot(f"{self.folderPath}/ {self.testTime}-test-retrofitSelenium-getAll.png"),
-            lambda:self.result(GC.ok)
+            lambda:self.result(Result.ok)
         ))
 
     def test_deleteAll(self):
-        self.runController(GC.deleteAll,(
+        self.runController(Product.deleteAll, (
             lambda:self.driver.save_screenshot(f"{self.folderPath}/ {self.testTime}-test-retrofitSelenium-getAll.png"),
-            lambda:self.result(GC.ok)
+            lambda:self.result(Result.ok)
         ))
     
     def test_updateProduct(self):
         self.makeProductIdNumber()
         updateInput="{\"description\":\"example1\",\"id\":"+ self.productIdNumber +",\"image\":\"example1\",\"price\":9.99,\"title\":\"example1\","+f"\"categoryId\":{self.categoryIdNumber}"+"}"
-        self.runControllerWithInput(GC.updateProduct,GC.body,updateInput,(
+        self.runController(Product.update,(
             lambda:self.driver.save_screenshot(f"{self.folderPath}/ {self.testTime}-test-retrofitSelenium-updateProduct.png"),
-            lambda:self.result(GC.ok)
-        ))
+            lambda:self.result(Result.ok)
+        ),Write.body,updateInput)
+
     def test_addProduct(self):
-        self.runController(GC.deleteAll,())
         self.makeCategoryIdNumber()
         addProductExaple="{\"description\":\"example\",\"image\":\"example\",\"price\":9.99,\"title\":\"example\","+f"\"categoryId\":{self.categoryIdNumber}"+"}"
-        self.runControllerWithInput(GC.addProduct,GC.body,addProductExaple,(
+        self.runController(Product.add,(
             lambda:self.driver.save_screenshot(f"{self.folderPath}/ {self.testTime}-test-retrofitSelenium-addProduct.png"),
-            lambda:self.result(GC.created)
-        ))
+            lambda:self.result(Result.created)
+        ),Write.body,addProductExaple)
 
     def test_getByQueryProductResponse(self):
-        self.runController(GC.deleteAll,())
-
-        self.runControllerWithInput(GC.getByQueryProductResponse,GC.parameters,"example",(
+        self.runController(Product.getByQuery,(
             lambda:self.driver.save_screenshot(f"{self.folderPath}/ {self.testTime}-test-retrofitSelenium-getByQueryProductResponse.png"),
-            lambda:self.result(GC.ok) 
-        ))
+            lambda:self.result(Result.ok) 
+        ),Write.parameters, Write.queryExample)
 
     def test_getByCategoryProductResponse(self):
-        self.runController(GC.deleteAll,())
         self.makeCategoryIdNumber()
-        self.runControllerWithInput(GC.getByCategoryProductResponse,GC.parameters,self.categoryIdNumber,(
+        self.runController(Product.getByCategory,(
             lambda:self.driver.save_screenshot(f"{self.folderPath}/ {self.testTime}-test-retrofitSelenium-getByCategoryProductResponse.png"),
-            lambda:self.result(GC.ok) 
-        ))
+            lambda:self.result(Result.ok)
+        ),Write.parameters,self.categoryIdNumber)
     def test_deleteProduct(self):
-        self.runController(GC.deleteAll,())
         self.makeProductIdNumber()
-        self.runControllerWithInput(GC.deleteProduct,GC.parameters,self.productIdNumber,(
+        self.runController(Product.delete,(
             lambda:self.driver.save_screenshot(f"{self.folderPath}/ {self.testTime}-test-retrofitSelenium-deleteProduct.png"),
-            lambda:self.result(GC.ok) 
-        ))
+            lambda:self.result(Result.ok)
+        ),Write.parameters,self.productIdNumber)
